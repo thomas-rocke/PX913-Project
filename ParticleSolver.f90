d25f58088e81beb0151c273f6cc212804d305229
Thomas Rocke <45517493+thomas-rocke@users.noreply.github.com>
2021-11-29 16:18:49 +0000

    Create ParticleSolver.f90

---
+++ ParticleSolver.f90
@@ -0,0 +1,13 @@
+! Module that uses verlet method to find the trajectory of electrons
+! Given electric field, and initial position and velocity
+
+
+module ParticleSolver
+  implicit none
+
+
+
+
+
+
+end module ParticleSolver
