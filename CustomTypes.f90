b812c75d3f8dc226c829192bb23c294394b23af4
Thomas Rocke <45517493+thomas-rocke@users.noreply.github.com>
2021-11-29 17:39:13 +0000

    Update and rename CustomTypes.f90 to GlobalUtils.f90

--- CustomTypes.f90
+++
@@ -1,8 +0,0 @@
-! Define some custom types required for project
-
-
-module CustomTypes
-  implicit none
-  
- 
-end module CustomTypes
---
+++ GlobalUtils.f90
@@ -0,0 +1,10 @@
+! Define some useful types and procedures, for use throughout the project
+
+! Particle and FieldData custom types
+! Methods to estimate Grad in x- and y-components, and grad^2
+
+module GlobalUtils
+  implicit none
+  
+ 
+end module GlobalUtils
